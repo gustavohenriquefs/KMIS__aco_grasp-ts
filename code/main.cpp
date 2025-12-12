a5644f3249e585e1d4236947a1b6a7df1f5c1541
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2025-12-12 10:46:58 -0300

    refactor: remove excessive logging from ACO and GRASP+TS processing functions

--- code/main.cpp
+++ code/main.cpp
@@ -10,25 +10,19 @@
 // Function to process ACO for a given instance
 // @param instance The instance to process
 void processACO(const Instance& instance, ReportManager& report_manager) {
-  std::cerr << "  [LOG] Iniciando ACO para: " << instance.get_file_name() << std::endl;
-
   ACOKMIS aco_kmis = ACOKMIS(
       instance.get_connections(),
       instance.get_num_elements_l(),
       instance.get_num_elements_r());
 
-  std::cerr << "  [LOG] Executando solve_kMIS com k=" << instance.get_k() << std::endl;
   auto exec_reports = aco_kmis.solve_kMIS(instance.get_k());
-  std::cerr << "  [LOG] solve_kMIS finalizado. Reports gerados: " << exec_reports.size() << std::endl;
 
   Report report_instance(instance.get_connections(),
                          instance.get_file_name(),
                          instance.get_k(),
                          exec_reports);
 
-  std::cerr << "  [LOG] Adicionando report ao manager..." << std::endl;
   report_manager.add_reports(report_instance);
-  std::cerr << "  [LOG] Report adicionado com sucesso!" << std::endl;
 }
 
 InstanceI mapACOInstanceToGRASPTsInstance(const Instance& i) {
@@ -48,28 +42,22 @@ InstanceI mapACOInstanceToGRASPTsInstance(const Instance& i) {
 // Function to process GRASP+Tabu Search for a given instance
 // @param instance The instance to process
 void processGRASPTs(const Instance& instance, ReportManager& report_manager) {
-  std::cerr << "  [LOG] Iniciando GRASP+TS para: " << instance.get_file_name() << std::endl;
-
   InstanceI I = mapACOInstanceToGRASPTsInstance(instance);
 
   vector<ReportExecData> results;
 
   for (int iter = 0; iter < 10; iter++) {
     GRASPTs graspts = GRASPTs(I);
-    std::cerr << "    [LOG] GRASP iteracao " << (iter + 1) << "/10" << std::endl;
     auto result = graspts.solve_kMIS();
     results.insert(results.end(), result.begin(), result.end());
   }
-
-  std::cerr << "  [LOG] GRASP finalizado. Reports gerados: " << results.size() << std::endl;
 
   Report report_instance(instance.get_connections(),
                          instance.get_file_name(),
                          instance.get_k(),
                          results);
-  std::cerr << "  [LOG] Adicionando report ao manager..." << std::endl;
+
   report_manager.add_reports(report_instance);
-  std::cerr << "  [LOG] Report adicionado com sucesso!" << std::endl;
 }
 
 int main() {
@@ -78,38 +66,23 @@ int main() {
   std::cin.tie(nullptr);
 #endif
 
-  std::cerr << "======================================" << std::endl;
-  std::cerr << "[LOG] Iniciando programa..." << std::endl;
-  std::cerr << "======================================" << std::endl;
-
   IntancesReader reader = IntancesReader();
   const auto& instances = reader.get_instances();
-
-  std::cerr << "[LOG] Total de instancias carregadas: " << instances.size() << std::endl;
 
   ReportManager report_manager_graspts = ReportManager("graspts");
 
   int instance_idx = 0;
   for (auto instance : instances) {
     instance_idx++;
-    std::cerr << "[LOG] Processando instancia GRASP " << instance_idx << "/" << instances.size()
-              << ": " << instance.get_file_name() << std::endl;
 
     processGRASPTs(instance, report_manager_graspts);
   }
-
-  std::cerr << "-----------------acabou graspts-----------------------" << std::endl;
 
   ReportManager report_manager_aco = ReportManager("aco_kmis");
 
   instance_idx = 0;
   for (auto instance : instances) {
     instance_idx++;
-    std::cerr << "[LOG] Processando instancia ACO " << instance_idx << "/" << instances.size()
-              << ": " << instance.get_file_name() << std::endl;
-
     processACO(instance, report_manager_aco);
   }
-  std::cerr << "-----------------acabou-----------------------" << std::endl;
-  std::cerr << "[LOG] Programa finalizado com sucesso!" << std::endl;
 }
